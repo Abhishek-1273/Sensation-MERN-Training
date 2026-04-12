2cc92921f73d39527b815c0857b25e81f3dfeb27
Abhishek <abhi.l127375@gmail.com>
2026-04-12 10:30:05 +0530

    Daily Track of Backend

---
+++ BACKEND/Day-8-Backend-Revision/server.js
@@ -0,0 +1,13 @@
+import app from "./app.js";
+import { db } from "./config/db.js";
+
+// -------------------- CONFIG --------------------
+const PORT = process.env.PORT || 3000;
+
+// -------------------- DATABASE --------------------
+db(); 
+
+// -------------------- SERVER START --------------------
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
