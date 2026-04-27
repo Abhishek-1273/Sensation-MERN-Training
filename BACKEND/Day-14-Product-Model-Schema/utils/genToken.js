b7e7828cae93b37b1a1ef4cad3a663ef944b5f5e
Abhishek <abhi.l127375@gmail.com>
2026-04-27 21:56:25 +0530

    Delete BACKEND directory

--- BACKEND/Day-14-Product-Model-Schema/utils/genToken.js
+++
@@ -1,7 +0,0 @@
-import jwt from "jsonwebtoken";
-
-export const genToken = (id, name, role) => {
-    return jwt.sign({ id, name, role }, process.env.JWT_SECRET, {
-        expiresIn: "1d",
-    });
-};
